f513e0c64f705fd993e2249e529378aeb86484fd
DannyNiu/NJF <dannyniu@hotmail.com>
2024-06-28 17:15:27 +0800

    Branch development put on hold indefinitely.

--- src/s2i.h
+++ src/s2i.h
@@ -5,6 +5,28 @@
 
 #include "SafeTypes2.h"
 
+// 2024-06-28:
+// ==========
+//
+// The "i" stand for "implicit" operand, "improve".
+//
+// While it was intended as an improvement, advice were received on
+// StackOverflow saying that this could lead to fragmentation of the use of API
+// and that the supposed benefit offered by it is trivial that of the original
+// API, especially taken into account when the tooling used by developer
+// may not be capable of parsing a "made-up" syntax such as that from this
+// header.
+//
+// What's more, idioms envisioned for this header - i.e. Rust-like unwrap chain
+// require error and type checking. For example,
+// the fragment ``dict[key].array_shift().map()'' require an implementation of
+// the shift function to check for list type, and a map that checks for
+// data blob type. Since none of these were ever planned (as their benefit and
+// drawbacks are far from known), the implementation of this idiom is
+// put on hold indefinitely, and this branch should be considered abandoned.
+//
+// THE USE OF THIS HEADER AND ITS INTERFACE IS AT YOUR OWN RISK.
+
 typedef s2obj_t *obj;
 
 #define s2i_decl obj _
